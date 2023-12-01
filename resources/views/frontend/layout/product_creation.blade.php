14bf2fbaf67fce55aeadc16040b8cfcbe3697032
Sanjay Kumar <sk963070@gmail.com>
2023-12-01 16:10:50 +0530

    new changes

--- resources/views/frontend/layout/product_creation.blade.php
+++ resources/views/frontend/layout/product_creation.blade.php
@@ -6,15 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CauseStand</title>
     <!-- stylesheet  -->
-    <link rel="stylesheet" href="{{url('/')}}/asset/frontend/css/bootstrap.min.css">
-
     <!--
         <link rel="stylesheet" href="asset/frontend/css/stylesheet.css">
         <link rel="stylesheet" href="asset/frontend/css/responsive.css">
     -->
 
+    <link rel="stylesheet" href="{{url('/')}}/asset/frontend/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{url('/')}}/asset/frontend/css/responsive.css">
+    <link rel="stylesheet" href="{{url('/')}}/asset/frontend/css/stylesheet.css">
     <link rel="stylesheet" href="{{url('/')}}/asset/frontend/css/final-style.css">
     <link rel="stylesheet" href="{{url('/')}}/asset/frontend/css/final-responsive.css">
+
     
     <!-- font-awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
