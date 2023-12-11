b2aea4030afe7a67b62fa2d3164ce9db40e7cca1
Sanjay Kumar <sk963070@gmail.com>
2023-12-11 22:55:33 +0530

    new changes

--- resources/views/frontend/layout/product_creation.blade.php
+++ resources/views/frontend/layout/product_creation.blade.php
@@ -79,7 +79,7 @@
             <div class="row">
                 <div class="col-md-9">
                     <div class="left-side" id="logo-hide">
-                        <img onclick='location.href ="{{route('/')}}";' src="{{url('/')}}/asset/frontend/images/new-logo.jpg" alt="">
+                    <img onclick="window.location.href = '{{ route('home') }}';" src="{{ url('/asset/frontend/images/new-logo.jpg') }}" alt="">
                     </div>
                 </div>
                 <div class="col-lg-3 d-flex justify-content-end">
