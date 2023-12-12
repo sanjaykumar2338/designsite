07de348b114cb879cde26e0b708cd4ae9312ed13
Sanjay Kumar <sk963070@gmail.com>
2023-12-12 19:18:32 +0530

    new changes

--- resources/views/frontend/pages/create_product.blade.php
+++ resources/views/frontend/pages/create_product.blade.php
@@ -83,21 +83,21 @@
                         <div id="product-thumbnails">
                             
                             <button class="border rounded-lg" onclick="setShowCanvas(`canvas_front`, true)">
-                                <img id="thumbnail_front" src="" alt="" class="h-[50px] w-[50px]">
+                                <img width="50" height="50" id="thumbnail_front" src="" alt="" class="h-[50px] w-[50px]">
                                 {{-- front --}}
                             </button>
                             
                             <button class="border  rounded-lg" onclick="setShowCanvas(`canvas_back`, true)">
-                                <img id="thumbnail_back" src="" alt="" class="h-[50px] w-[50px]">
+                                <img width="50" height="50" id="thumbnail_back" src="" alt="" class="h-[50px] w-[50px]">
                                 {{-- back --}}
                             </button>
                             
                             <button class="border  rounded-lg" onclick="setShowCanvas(`canvas_sleeve_left`, true)">
-                                <img id="thumbnail_sleeve_left" src="" alt="" {{-- sleeve_left --}}>
+                                <img width="50" height="50" id="thumbnail_sleeve_left" src="" alt="" {{-- sleeve_left --}}>
                             </button>
 
                             <button class="border  rounded-lg" onclick="setShowCanvas(`canvas_sleeve_right`, true)">
-                                <img id="thumbnail_sleeve_right" src="" alt=""
+                                <img width="50" height="50" id="thumbnail_sleeve_right" src="" alt=""
                                     class="h-[50px] w-[50px]">
                                 {{-- sleeve_right --}}
                             </button>
