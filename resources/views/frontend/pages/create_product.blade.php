9bb1587c1ca598e5fb53ffdd31f69f03ee37bc81
Sanjay Kumar <sk963070@gmail.com>
2023-12-15 13:25:57 +0530

    new changes

--- resources/views/frontend/pages/create_product.blade.php
+++ resources/views/frontend/pages/create_product.blade.php
@@ -358,7 +358,7 @@
                     &#8203;
                 </span>
                 <div class="inline-block align-center bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
-                    role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                    role="dialog" style="max-width: 50rem;" aria-modal="true" aria-labelledby="modal-headline">
                     <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                         <label class="font-bold text-gray-800">Payment Details</label>
                         <br>
