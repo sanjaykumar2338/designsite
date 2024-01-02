9219ef9d064dc8b8bc4c3588cbe8151ac02c8d5a
JASH-JARIWALA <jashjariwala7911@gmail.com>
2024-01-02 18:35:36 +0530

    update

--- resources/views/frontend/pages/create_product.blade.php
+++ resources/views/frontend/pages/create_product.blade.php
@@ -412,7 +412,7 @@
     </div>
 
     <!-- SPINNER -->
-    <div id="loader" hidden style="" wire:loading
+    <div id="loader" hidden style="" wire:loading style="z-index: 1000"
         class="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
         <div style="border-top-color: #3498db !important"
             class="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
