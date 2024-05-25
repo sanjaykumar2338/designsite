7be040b960a3a6756c3306259a5f8e7586d1c95f
Sanjay Kumar <sk963070@gmail.com>
2024-05-25 15:21:27 +0530

    new changes

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\PrintfulOrder;
 use App\Models\Payment;
 class AdminController extends Controller{
     
@@ -18,8 +19,15 @@ class AdminController extends Controller{
     }
 
     public function order(){
-        $orders = Payment::join('users','users.id','=','payments.user_id')->select('payments.*','users.name')->paginate(5);
-        return view('admin.pages.order.index')->with('orders',$orders)->with('activeLink','order');
+       
+            $email = auth()->user()->email;
+            $orders = PrintfulOrder::join('users', 'users.email', '=', 'printful_orders.customer_email')->join('products', 'products.id', '=', 'printful_orders.product_id')->join('payments', 'payments.id', '=', 'printful_orders.payment_id')->select('products.*','printful_order_data','payment_intent_id','payments.amount as amt','products.supporting_country','products.product_for','products.product_type','printful_orders.total_amount','printful_orders.product_price','printful_orders.id')->orderBy('printful_orders.id','desc')->paginate(5);
+
+            return view('admin.pages.order.index')->with('activeLink', 'orders')->with('orders', $orders);
+        
+
+     //   $orders = Payment::join('users','users.id','=','payments.user_id')->select('payments.*','users.name')->paginate(5);
+       // return view('admin.pages.order.index')->with('orders',$orders)->with('activeLink','order');
     }
 
     public function customer(){
--- resources/views/admin/pages/order/index.blade.php
+++ resources/views/admin/pages/order/index.blade.php
@@ -68,25 +68,75 @@
                <div class="row">
                   <table class="table table-hover">
                      <thead>
-                        <tr>
-                           <th>#ID</th>
-                           <th>Customer</th>
-                           <th>Amount</th>
-                           <th>Stripe Payment ID</th>
-                           <th>Purchased On</th>
-                        </tr>
+                     <tr>
+                        <th scope="col">Printful #ID</th>
+                        <th scope="col">Product Name</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Total Amount</th>
+                        <th scope="col">Donation</th>
+                        <th scope="col">Country</th>
+                        <th scope="col">Payment ID</th>
+                        <th scope="col">Product</th>
+                        <th scope="col">Invoice</th>
+                     </tr>
                      </thead>
                      <tbody>
 
-                        @foreach($orders as $order)
-                            <tr>
-                                <td>{{$order->id}}</td>
-                                <td>{{$order->name}}</td>
-                                <td>{{$order->amount}}</td>
-                                <td>{{$order->payment_intent_id}}</td>
-                                <td>{{$order->created_at}}</td>
-                            </tr>
-                        @endforeach
+                     @if($orders) 
+                           @foreach($orders as $order) 
+                           
+                              <tr>
+                                 @php
+                                       $data = json_decode($order->printful_order_data, true);
+                                       $url = url('/').'/stand-with-'.strtolower($order->supporting_country).'/shop/'.strtolower($order->product_type).'/'.$order->product_slug;
+
+                                       $curl = curl_init();
+
+                                       curl_setopt_array(
+                                          $curl,
+                                          array(
+                                             CURLOPT_URL => 'https://api.printful.com/orders/' . $data['id'],
+                                             CURLOPT_RETURNTRANSFER => true,
+                                             CURLOPT_ENCODING => '',
+                                             CURLOPT_MAXREDIRS => 10,
+                                             CURLOPT_TIMEOUT => 0,
+                                             CURLOPT_FOLLOWLOCATION => true,
+                                             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                                             CURLOPT_CUSTOMREQUEST => 'GET',
+                                             CURLOPT_HTTPHEADER => array(
+                                                   'X-PF-Store-Id: 12631976',
+                                                   'Content-Type: application/json',
+                                                   'Accept: application/json',
+                                                   'Authorization: Bearer te6lqpl4ju9anm3y0TWtWTLaAVDiQz6ddtAspwJc',
+                                                   'Cookie: __cf_bm=b_KF_QLD5hwAjdrI5D..II41J0suVQ6rDItqE3fGpiU-1700675108-0-AWJ8qrjc2rORqrGTIYe7pXKj/XaMG6zJ3iQM8WjXiDaVgUvKW48NY6wf3+kjlL/tafcmaYGux6DA4EnogAZEYC8=; dsr_setting=%7B%22region%22%3A1%2C%22requirement%22%3Anull%7D'
+                                             ),
+                                          )
+                                       );
+
+                                       $response = curl_exec($curl);
+
+                                       curl_close($curl);
+                                       $info = json_decode($response, true);
+                                 @endphp
+
+                                 <th scope="row">{{$data['id']}}</th>
+                                 <td>{{$order->website_product_name}}</td>
+                                 <td>{{$info['result']['status']}}</td>
+                                 <td>${{$data['retail_costs']['total']}}</td>
+                                 <td>${{$order->total_amount - $order->product_price}}</td>
+                                 <td>{{$order->supporting_country}}</td>
+                                 <td>{{$order->payment_intent_id}}</td>
+                                 <td>
+                                       <button type="button" class="btn btn-primary" onclick="window.open('{{ $url }}', '_blank')"><i class="far fa-eye"></i></button>
+                                       <button style="display:none;" type="button" class="btn btn-success"><i class="fas fa-edit"></i></button>
+                                       <button style="display:none;" type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
+                                 </td>
+                                 <td>
+                                       <button type="button" class="btn btn-primary" onclick="window.open('{{ url('/invoice/' . $order->id) }}', '_blank')" style="cursor: pointer;"><i class="far fa-eye"></i></button>
+                                 </td>
+                              </tr>
+                           @endforeach
+                        @endif  
 
                      </tbody>
                   </table>
