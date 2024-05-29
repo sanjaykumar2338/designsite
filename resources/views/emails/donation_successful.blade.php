a879a4d861dd5086e11c8813e05a569f2b7141ac
Sanjay Kumar <sk963070@gmail.com>
2024-05-29 12:54:43 +0530

    new changes

--- resources/views/emails/donation_successful.blade.php
+++ resources/views/emails/donation_successful.blade.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Donation Successful</title>
+    <title>Donation Confirmation</title>
 </head>
 <body>
-    <h1>Thank you for your donation!</h1>
-    <p>Your donation of ${{ number_format($amount / 100, 2) }} has been successfully sent to {{ $country }}.</p>
-    <p>Thank you for supporting {{ $country }} through {{ $nonprofit }}.</p>
-    <p>We appreciate your generosity and support.</p>
+    <h1>Thank You for Your Donation!</h1>
+
+    <p>Your donation of ${{ number_format($amount / 100, 2) }} has been successfully sent to a nonprofit that supports {{ $country }}.</p>
+
+    <p>Thank you for supporting Israel through "{{ $nonprofit }}".</p>
+
+    <p>A receipt from "{{ $nonprofit }}" will be sent to your email within 30 days.</p>
+
+    <p>Your support is appreciated!</p>
+
+    <p>Cause Stand LLC</p>
+
+    <p>Supply Love, Demand Peace.</p>
 </body>
 </html>
