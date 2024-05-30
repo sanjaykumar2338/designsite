d1c62bc423948924ca3e522429304b15fa3b559a
Sanjay Kumar <sk963070@gmail.com>
2024-05-30 16:20:23 +0530

    new changes

--- resources/views/frontend/pages/my_account.blade.php
+++ resources/views/frontend/pages/my_account.blade.php
@@ -53,40 +53,12 @@
                 <tr>
                     @php
                         $data = json_decode($order->printful_order_data, true);
-                        $url = url('/').'/stand-with-'.strtolower($order->supporting_country).'/shop/'.strtolower($order->product_type).'/'.$order->product_slug;
-
-                        $curl = curl_init();
-
-                        curl_setopt_array(
-                            $curl,
-                            array(
-                                CURLOPT_URL => 'https://api.printful.com/orders/' . $data['id'],
-                                CURLOPT_RETURNTRANSFER => true,
-                                CURLOPT_ENCODING => '',
-                                CURLOPT_MAXREDIRS => 10,
-                                CURLOPT_TIMEOUT => 0,
-                                CURLOPT_FOLLOWLOCATION => true,
-                                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                                CURLOPT_CUSTOMREQUEST => 'GET',
-                                CURLOPT_HTTPHEADER => array(
-                                    'X-PF-Store-Id: 12631976',
-                                    'Content-Type: application/json',
-                                    'Accept: application/json',
-                                    'Authorization: Bearer te6lqpl4ju9anm3y0TWtWTLaAVDiQz6ddtAspwJc',
-                                    'Cookie: __cf_bm=b_KF_QLD5hwAjdrI5D..II41J0suVQ6rDItqE3fGpiU-1700675108-0-AWJ8qrjc2rORqrGTIYe7pXKj/XaMG6zJ3iQM8WjXiDaVgUvKW48NY6wf3+kjlL/tafcmaYGux6DA4EnogAZEYC8=; dsr_setting=%7B%22region%22%3A1%2C%22requirement%22%3Anull%7D'
-                                ),
-                            )
-                        );
-
-                        $response = curl_exec($curl);
-
-                        curl_close($curl);
-                        $info = json_decode($response, true);
+                        $url = url('/').'/stand-with-'.strtolower($order->supporting_country).'/shop/'.strtolower($order->product_type).'/'.$order->product_slug;                        
                     @endphp
 
                     <th scope="row">{{$data['id']}}</th>
                     <td>{{$order->website_product_name}}</td>
-                    <td>{{$info['result']['status']}}</td>
+                    <td>{{$data['printful_order_status']}}</td>
                     <td>${{$data['retail_costs']['total']}}</td>
                     <td>${{$order->total_amount - $order->product_price}}</td>
                     <td>{{$order->supporting_country}}</td>
