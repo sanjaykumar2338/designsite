a91ffdc84f5252f399861728ca42f8aeecfcffc0
Sanjay Kumar <sk963070@gmail.com>
2024-06-01 13:40:56 +0530

    predesign

--- resources/views/emails/donation_successful.blade.php
+++ resources/views/emails/donation_successful.blade.php
@@ -6,11 +6,11 @@
 <body>
     <h1>Thank You for Your Donation!</h1>
 
+    <p>This donation was made to {{ $nonprofit }} via Our Change Foundation 501(c)(3) (EIN: 86-3353394). No goods or services were provided in exchange for this donation.</p>
+
     <p>Your donation of ${{ number_format($amount / 100, 2) }} has been successfully sent to a nonprofit that supports {{ $country }}.</p>
 
     <p>Thank you for supporting Israel through "{{ $nonprofit }}".</p>
-
-    <p>A receipt from "{{ $nonprofit }}" will be sent to your email within 30 days.</p>
 
     <p>Your support is appreciated!</p>
 
