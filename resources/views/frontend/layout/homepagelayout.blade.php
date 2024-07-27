96aa4e480e51c4cc8b022f8ac0ee0ad1b41e649d
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2024-07-27 15:18:16 +0530

    new changes

--- resources/views/frontend/layout/homepagelayout.blade.php
+++ resources/views/frontend/layout/homepagelayout.blade.php
@@ -54,7 +54,7 @@
                        
                         @if (Auth::check())
                             <li>
-                                <span style="cursor: pointer;" class="add-border" onclick='location.href ="{{url('myaccount')}}";'>
+                                <span style="cursor: pointer;" class="add-border" onclick='location.href ="{{url('dashboard')}}";'>
                                     My Account
                                 </span>
                             </li>
