3bccd7f6dc11388db4c464988131eedc3645d20a
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2024-08-02 21:45:10 +0530

    aut

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -45,7 +45,11 @@ class UserController extends Controller
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
-            return redirect()->route('home')->with('success', 'Login successful.');
+            if (auth()->user()->email == 'admin@gmail.com') {
+                return redirect('admin');
+            } else {
+                return redirect()->route('home')->with('success', 'Login successful.');
+            }
         }
 
         return back()->withErrors([
