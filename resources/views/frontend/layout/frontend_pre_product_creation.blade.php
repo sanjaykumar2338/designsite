5dd315defc96581d402bf4965babfaa6369cf08a
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2024-10-26 13:38:11 +0530

    step schedule tour for date and time

--- resources/views/frontend/layout/frontend_pre_product_creation.blade.php
+++ resources/views/frontend/layout/frontend_pre_product_creation.blade.php
@@ -215,7 +215,54 @@
                             <a class="nav-link" href="{{route('causes')}}">Causes</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{route('collections')}}">Shop</a>
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Shop <b class="caret"></b></a>
+
+                            <ul class="dropdown-menu">
+                                <li class="nav-item">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Shop by Collection <b class="caret"></b></a>
+
+                                    <ul class="dropdown-menu" style="">
+                                        @foreach(\App\Models\Collections::all() as $collection)
+                                        <li>
+                                            <a href="{{url('/')}}/shop/{{$collection->slug}}-collection" class="" data-toggle="dropdown">{{$collection->title}} Collection</a>
+                                        </li>
+                                        @endforeach
+                                    </ul>
+                                </li>
+								<li class="nav-item">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Shop by Product Type <b class="caret"></b></a>
+
+                                    <ul class="dropdown-menu" style="">
+                                        <li>
+                                            <a href="#" class="" data-toggle="dropdown">T-Shirts  <b class="caret"></b></a>
+                                        </li>
+										<li>
+                                            <a href="#" class="" data-toggle="dropdown">Hoodies    <b class="caret"></b></a>
+                                        </li>
+										<li>
+                                            <a href="#" class="" data-toggle="dropdown">Sweatshirts  <b class="caret"></b></a>
+                                        </li>
+                                    </ul>
+                                </li>
+								<li class="nav-item">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Shop by Design <b class="caret"></b></a>
+
+                                    <ul class="dropdown-menu" style="">
+                                        <li>
+                                            <a href="#" class="" data-toggle="dropdown">Oversight Collection   <b class="caret"></b></a>
+                                        </li>
+										<li>
+                                            <a href="#" class="" data-toggle="dropdown">Traitor  Collection   <b class="caret"></b></a>
+                                        </li>
+										<li>
+                                            <a href="#" class="" data-toggle="dropdown">Trader  Collection   <b class="caret"></b></a>
+                                        </li>
+										<li>
+                                            <a href="#" class="" data-toggle="dropdown">Propaganda   Collection   <b class="caret"></b></a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{route('media')}}">Media</a>
