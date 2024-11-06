55362827ea877b9b4ea37cac943a492332620e1f
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2024-11-06 15:56:44 +0530

    new changes

--- resources/views/frontend/pages/pre_product_list.blade.php
+++ resources/views/frontend/pages/pre_product_list.blade.php
@@ -103,7 +103,7 @@
         @if($boycott->count() > 0)
             <div class="row row-cols-1 row-cols-md-3 g-4">
                 @foreach($boycott as $bt) 
-                    @if(file_exists(fileToUrl($bt->blog_image)))
+                    @if(isset($bt->blog_image) && isset($bt->title))
                         <div class="col">
                             <div class="card h-100">
                                 <img src="{{ fileToUrl($bt->blog_image) }}" class="card-img-top" alt="{{ $bt->title }}">
