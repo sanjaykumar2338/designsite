d6cf0ec1a94dd751b7c21eefec3a3a480c0f5e3a
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2024-11-13 14:05:23 +0530

    new changes

--- resources/views/frontend/pages/pre_product_list.blade.php
+++ resources/views/frontend/pages/pre_product_list.blade.php
@@ -160,7 +160,7 @@
 
 <div class="container my-4">
     @if($boycott->count() > 0)
-        <div class="row row-cols-1 row-cols-md-4 g-4">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
             @foreach($boycott as $bt)
                 @if(isset($bt->blog_image) && isset($bt->title) && fileToUrl($bt->blog_image) != 'https://causestand.com/storage/')
                     <div class="col">
