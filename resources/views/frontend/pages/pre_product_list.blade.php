fb9ea002f9ff4d5ea018332346107f51c31a840d
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2024-11-14 08:54:19 +0530

    new changes

--- resources/views/frontend/pages/pre_product_list.blade.php
+++ resources/views/frontend/pages/pre_product_list.blade.php
@@ -259,7 +259,7 @@
                                         preloadImage(overlay, function(overlayImg) {
                                             overlayImg.set({
                                                 left: canvas.width / 2,
-                                                top: canvas.height / 5,
+                                                top: canvas.height / 1,
                                                 originX: 'center',
                                                 originY: 'center',
                                                 selectable: false
