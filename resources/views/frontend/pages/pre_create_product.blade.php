05eb8453e3d9d9116c497a0bac7d4fbce8ddaa85
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2024-11-18 11:36:00 +0530

    new changes

--- resources/views/frontend/pages/pre_create_product.blade.php
+++ resources/views/frontend/pages/pre_create_product.blade.php
@@ -129,7 +129,7 @@
                     @endphp
 
                     <div class="desc_data">
-                        {!! nl2br(e($boycott->description)) !!}
+                        {!! $boycott->description !!}
                     </div>
 
                     <div class="prd-option">
