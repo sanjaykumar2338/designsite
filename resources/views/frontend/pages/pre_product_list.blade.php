d4c883af057bb1fb550fc257e320978990a13a16
Sanjay Kumar <sanjaykumar@Sanjays-MacBook-Air.local>
2024-11-20 22:16:11 +0530

    new chagnes

--- resources/views/frontend/pages/pre_product_list.blade.php
+++ resources/views/frontend/pages/pre_product_list.blade.php
@@ -206,8 +206,8 @@
                                 }
 
                                 if('{{$design_type}}'=='hoodies'){
-                                    back_image_size = 4.8;
-                                    width = 0.45;
+                                    back_image_size = 2.8;
+                                    width = 0.35;
                                     var frontBackground = 'https://files.cdn.printful.com/m/g18500/medium/ghost/front/05_gildan18500_ghost_front_base_whitebg.png?v=1700731048';
                                     var backBackground = 'https://files.cdn.printful.com/m/g18500/medium/ghost/back/05_gildan18500_ghost_back_base_whitebg.png?v=1700731048';
                                 }
